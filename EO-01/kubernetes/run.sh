325f68f59a0532644951d466b664e5a161d49dd3
Ubuntu <fabian.lehmann@informatik.hu-berlin.de>
2021-03-30 12:52:15 +0000

    updated run command

--- EO-01/kubernetes/run.sh
+++ EO-01/kubernetes/run.sh
@@ -1,1 +1,6 @@
-nextflow kuberun /workdir/B5-Workflow-Earth-Observation/EO-01/workflow.nf -v ceph-fs-volume:/workdir -v fonda-datasets:/data -profile kubernetesConf --inputdata /data/b5/eo-01/
+nextflow kuberun /workdir/B5-Workflow-Earth-Observation/EO-01/workflow-dsl2.nf \
+-v ceph-fs-volume:/workdir \
+-v fonda-datasets:/data \
+-profile kubernetesConf \
+--inputdata /data/b5/eo-01/ \
+--outdata /workdir/output/
