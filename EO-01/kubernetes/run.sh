f41a77ec5b920f00fca24a0ecf4af10a688cbe0f
Ubuntu <fabian.lehmann@informatik.hu-berlin.de>
2021-04-14 10:14:51 +0000

    changed kubernetes parameters

--- EO-01/kubernetes/run.sh
+++ EO-01/kubernetes/run.sh
@@ -2,5 +2,8 @@ nextflow kuberun /workdir/B5-Workflow-Earth-Observation/EO-01/workflow-dsl2.nf \
 -v ceph-fs-volume:/workdir \
 -v fonda-datasets:/data \
 -profile kubernetesConf \
+-queue-size 30 \
 --inputdata /data/b5/eo-01/ \
---outdata /workdir/output/
+--outdata /workdir/output/ \
+-pod-image fabianlehmann/nextflow:fix \
+-resume
