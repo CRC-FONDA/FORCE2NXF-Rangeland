ad4f757c0cf0f1619ea3efd5fe60b1c78b48c3e9
davidfrantz <frantz.feut@gmail.com>
2021-06-11 17:57:30 +0200

    update original

--- original/force-l2ps-params.sh
+++ original/force-l2ps-params.sh
@@ -2,6 +2,14 @@
 
 # set parameters - this is usually done by hand
 PARAM=$1
+CUBE=$2
+
+# read grid definition
+CRS=$(sed '1q;d' $CUBE)
+ORIGINX=$(sed '2q;d' $CUBE)
+ORIGINY=$(sed '3q;d' $CUBE)
+TILESIZE=$(sed '6q;d' $CUBE)
+BLOCKSIZE=$(sed '7q;d' $CUBE)
 
 # set parameters
 sed -i "/^FILE_QUEUE /cFILE_QUEUE = queue.txt" $PARAM
@@ -11,6 +19,13 @@ sed -i "/^DIR_TEMP /cDIR_TEMP = tmp/" $PARAM
 sed -i "/^FILE_DEM /cFILE_DEM = dem/global_srtm-aster.vrt" $PARAM
 sed -i "/^DIR_WVPLUT /cDIR_WVPLUT = wvdb" $PARAM
 sed -i "/^FILE_TILE /cFILE_TILE = tiles.txt" $PARAM
-sed -i "/^NTHREAD /cNTHREAD = 56" $PARAM
+sed -i "/^TILE_SIZE /cTILE_SIZE = $TILESIZE" $PARAM
+sed -i "/^BLOCK_SIZE /cBLOCK_SIZE = $BLOCKSIZE" $PARAM
+sed -i "/^ORIGIN_LON /cORIGIN_LON = $ORIGINX" $PARAM
+sed -i "/^ORIGIN_LAT /cORIGIN_LAT = $ORIGINY" $PARAM
+sed -i "/^PROJECTION /cPROJECTION = $CRS" $PARAM
+sed -i "/^NPROC /cNPROC = 112" $PARAM
+sed -i "/^NTHREAD /cNTHREAD = 10" $PARAM
+sed -i "/^DELAY /cDELAY = 0" $PARAM
 
 exit 0
--- original/force-original.sh
+++ original/force-original.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # original FORCE workflow
+set -e
 
 BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -18,31 +19,37 @@ mkdir -p mask ard log tmp trend
 cp datacube-definition.prj mask/
 
 # generate processing masks
-docker-force force-cube aoi.gpkg mask rasterize 30
+echo "START: generate masks"
+time docker-force force-cube aoi.gpkg mask rasterize 30
 
 # generate a tile allow-list
-docker-force force-tile-extent aoi.gpkg mask tiles.txt
+echo "START: tile allow-list"
+time docker-force force-tile-extent aoi.gpkg mask tiles.txt
 
 # Level 2 parameter file
+echo "START: Level 2 Processing"
 docker-force force-parameter . LEVEL2 0
 mv LEVEL2-skeleton.prm ard.prm
-$BIN/force-l2ps-params.sh ard.prm # usually done by hand
+$BIN/force-l2ps-params.sh ard.prm datacube-definition.prj # usually done by hand
 
 # preprocessing to Level 2 ARD
-docker-force force-level2 ard.prm
+time docker-force force-level2 ard.prm
 
 # higher level (TSA) parameter file
+echo "START: Higher Level Processing"
 docker-force force-parameter . TSA 0
 mv TSA-skeleton.prm trend.prm
-$BIN/force-hlps-params.sh trend.prm # usually done by hand
+$BIN/force-hlps-params.sh trend.prm tiles.txt # usually done by hand
 
 # higher level processing
-docker-force force-higher-level trend.prm
+time docker-force force-higher-level trend.prm
 
 # mosaicking
-docker-force force-mosaic trend
+echo "START: mosaicking"
+time docker-force force-mosaic trend
 
 # pyramids
-docker-force force-pyramid trend/mosaic/*
+echo "START: pyramids"
+time docker-force force-pyramid trend/mosaic/*
 
 exit 0
