9d9e7963e1a9af500b241f6d3d9662e511d3f0fc
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2021-06-15 10:09:47 +0200

    clear local execution artefacts

--- EO-01/experiment/collectResults.sh
+++ EO-01/experiment/collectResults.sh
@@ -7,3 +7,5 @@ kubectl cp default/ceph-pod:/workdir/output/dag.dot ./dag.dot
 kubectl cp default/ceph-pod:/workdir/output/report.html ./results/$1/$2/report.html
 kubectl cp default/ceph-pod:/workdir/output/timeline.html ./results/$1/$2/timeline.html
 kubectl cp default/ceph-pod:/workdir/output/trace.txt ./results/$1/$2/trace.txt
+
+cp .nextflow.* ./results/$1/$2/
