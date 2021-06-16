8ea2d8ecc0c7a50053d181864e2f7d03af6b2a45
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2021-06-16 12:53:26 +0200

    Use runname instead of labels

--- EO-01/experiment/run.sh
+++ EO-01/experiment/run.sh
@@ -12,4 +12,5 @@ nextflow kuberun /workdir/B5-Workflow-Earth-Observation/EO-01/workflow-dsl2.nf \
 -with-dag /workdir/output/dag.dot \
 -with-report /workdir/output/report.html \
 -with-timeline /workdir/output/timeline.html \
--with-trace /workdir/output/trace.txt
+-with-trace /workdir/output/trace.txt \
+-name eo-experiment-test-n$1-e$2
--- EO-01/experiment/setupConfiguration.sh
+++
@@ -1,1 +0,0 @@
-sed -i "/process.pod = /c\\     process.pod = [ label : 'eo-experiment', value : 'n-$1_e-$2' ]" nextflow.config
