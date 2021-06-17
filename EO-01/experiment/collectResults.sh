6a9b0e97161a208c6fc1c1602fed6a5fd1182f78
Ubuntu <fabian.lehmann@informatik.hu-berlin.de>
2021-06-17 08:10:54 +0000

    collect logs in directory of the user

--- EO-01/experiment/collectResults.sh
+++ EO-01/experiment/collectResults.sh
@@ -1,7 +1,7 @@
 
 mkdir -p ./results/$1/$2/
 
-kubectl cp default/ceph-pod:/workdir/fabian/.nextflow.log ./results/$1/$2/logs.log
+kubectl cp default/ceph-pod:/workdir/$USER/.nextflow.log ./results/$1/$2/logs.log
 kubectl cp default/ceph-pod:/workdir/output/dag.dot ./results/$1/$2/dag.dot
 #convert dag to pdf / html
 kubectl cp default/ceph-pod:/workdir/output/report.html ./results/$1/$2/report.html
