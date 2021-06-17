cd490274ecc6ba0aee6947c3943e6e8a5bae965c
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2021-06-17 10:27:34 +0200

    Rename run scripts

--- EO-01/experiment/runKubernetesExperiment.sh
+++ EO-01/experiment/runKubernetesExperiment.sh
@@ -8,7 +8,7 @@ bash clearEnvironment.sh
 bash labelNodes.sh $1
 
 #execution phase
-bash run.sh $1 $2 > ./results/$1/$2/execution.log
+bash runOnKubernetes.sh $1 $2 > ./results/$1/$2/execution.log
 
 #finish phase
 bash collectResults.sh $1 $2
