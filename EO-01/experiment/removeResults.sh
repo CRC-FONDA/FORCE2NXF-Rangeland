ec65ea4163c6b4e04eb6a55777a63560ce9b4ad7
Ubuntu <fabian.lehmann@informatik.hu-berlin.de>
2021-06-17 09:07:11 +0000

    remove results in users directory

--- EO-01/experiment/removeResults.sh
+++ EO-01/experiment/removeResults.sh
@@ -1,5 +1,5 @@
 kubectl exec ceph-pod -n default --  rm /workdir/output -r
-kubectl exec ceph-pod -n default --  rm /workdir/fabian -r
+kubectl exec ceph-pod -n default --  rm /workdir/$USER -r
 kubectl exec ceph-pod -n default --  rm /workdir/work -r
 rm .nextflow -r
 rm .nextflow.log*
