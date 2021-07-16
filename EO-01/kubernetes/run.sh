2f013f14bcc280844ba5c28437b9be3d5fffabb4
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2021-07-16 10:39:32 +0200

    deleted nextflow.config and run.sh

--- EO-01/kubernetes/run.sh
+++
@@ -1,9 +0,0 @@
-nextflow kuberun /workdir/B5-Workflow-Earth-Observation/EO-01/workflow-dsl2.nf \
--v ceph-fs-volume:/workdir \
--v fonda-datasets:/data \
--profile kubernetesConf \
--queue-size 30 \
---inputdata /data/b5/eo-01/ \
---outdata /workdir/output/ \
--pod-image fabianlehmann/nextflow:fix \
--resume
