f4fef826b9b4318019f84966ac7109bfab5dc297
Felix Kummer <felix.kummer@hu-berlin.de>
2023-08-08 11:54:49 +0200

    Removal of remaining bio-related logic

--- lib/WorkflowRangeland.groovy
+++ lib/WorkflowRangeland.groovy
@@ -10,14 +10,6 @@ class WorkflowRangeland {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        /*
-        genomeExistsError(params, log)
-
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }*/
     }
 
     //
@@ -62,17 +54,5 @@ class WorkflowRangeland {
         def description_html = engine.createTemplate(methods_text).make(meta)
 
         return description_html
-    }//
-    // Exit pipeline if incorrect --genome key provided
-    //
-    private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
     }
 }
